10d0a994cadb89aa6a4640051dfeaa780274cea5
Amy Smith <amicloud.as@gmail.com>
2019-01-26 22:49:05 -0800

    According to all known laws of aviation

--- allan.js
+++ allan.js
@@ -2,7 +2,7 @@
 
 const Discord = require('discord.io');
 
-let delay = 5000;
+let delay = 3000;
 
 let allan1 = new Discord.Client({
     token: "NTM4NTY3Njk3NzY4Nzc1Njgy.Dy1wYA.A-e4v16ne4W3mt8LJmrsbbsEvxE",
@@ -16,6 +16,12 @@ let allan2 = new Discord.Client({
 let timeouts = [];
 let allans = [allan1, allan2];
 
+let channels =
+    ["538965173629747220", "538566527738445851", "538967327450988567",
+        "538967346094669825", "538967361491959811", "538967379926056972",
+        "538967392802570252", "538967408619290636", "538967464399339520",
+        "538967482996883476", "538967500982059054"];
+
 for (let allan of allans) {
     console.log("allan please login");
 
@@ -24,26 +30,23 @@ for (let allan of allans) {
     });
 
     allan.on('message', function (user, userID, channelID, message, event) {
-        console.log(`Message received: "${message}"`);
-        console.log(channelID);
-        if (channelID === "538566527738445851") {
+        if (channels.includes(channelID)) {
             if (message.match("RUINED IT AT")) {
-                throw(new DOMException("help"));
+                throw(new Error("Shit!"));
             }
             if (allan === allans[1]) {
                 if (userID !== allans[0].id) return;
             }
             if (userID === allan.id || userID === "510016054391734273") return;
-            console.log(`${allan.username} responding`);
-            if (parseInt(message) < 1014) {
+            if (parseInt(message) < 1200) {
                 let t = setTimeout(() => {
-                        let newMessage = `${parseInt(message) + 1}`;
-                        console.log(`${allan.username} sending message ${newMessage}`);
-                        allan.sendMessage({
-                            to: channelID,
-                            message: parseInt(message) + 1
-                        });
-                    }, delay);
+                    let newMessage = `${parseInt(message) + 1}`;
+                    console.log(`${allan.username} sending message ${newMessage}`);
+                    allan.sendMessage({
+                        to: channelID,
+                        message: parseInt(message) + 1
+                    });
+                }, delay);
                 timeouts.push(t);
             }
         }
